7be17c4cdce2bfcc75e4ebc1cb592fb8fcb71491
lazio <ahaffner75@gmail.com>
2025-10-07 15:19:33 +0200

    Code vom Controller hinzugefügt.

---
+++ backend/src/main/java/de/bootcamp/backend/controller/ExampleController.java
@@ -0,0 +1,10 @@
+package de.bootcamp.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/example")
+public class ExampleController {
+
+}
